6a3d8d0fe397bafe31ecf4933220d83e9a59fc8c
Nicholas Weaver <ncweaver@ucdavis.edu>
2024-10-09 20:47:52 +0000

    Modification to testing code: Now you won't get segfaults on partial completion.

--- httprequest_test.cpp
+++ httprequest_test.cpp
@@ -4,17 +4,21 @@
 
 // Well, this test actually isn't that simple...
 // It tests that the header parsing is correct:
-// the key should be lower case, but the value 
+// the key should be lower case, but the value
 // should be as is but trimmed for whitespace (isspace)
-TEST(HTTPRequestTest, TestSimple) {
+TEST(HTTPRequestTest, TestSimple)
+{
   HTTPRequest request("GET / HTTP/1.1\r\nHoST  : www.fubar.com\t\tUgh\t\r\n\r\n");
   EXPECT_EQ(request.get_command(), "GET");
-  EXPECT_EQ(request.get_headers()["host"]->get_value(), "www.fubar.com\t\tUgh");
+
   EXPECT_TRUE(request.get_headers().contains("host"));
   EXPECT_FALSE(request.get_headers().contains("HoST"));
+  if (request.get_headers().contains("host"))
+    EXPECT_EQ(request.get_headers()["host"]->get_value(), "www.fubar.com\t\tUgh");
 }
 
-TEST(HTTPRequestTest, TestPayload) {
+TEST(HTTPRequestTest, TestPayload)
+{
   HTTPRequest r("POST /fubar HTTP/1.1\r\nHost: aoeaoeu\r\n\r\nPayload!");
   EXPECT_EQ(r.get_command(), "POST");
   EXPECT_EQ(r.get_resource(), "/fubar");
@@ -23,18 +27,19 @@ TEST(HTTPRequestTest, TestPayload) {
   EXPECT_EQ(s.get_payload(), "Payload!  \r\n\r\n\r\nhey!");
 }
 
-TEST(HTTPRequestTest, TestExceptions){
+TEST(HTTPRequestTest, TestExceptions)
+{
   EXPECT_THROW(HTTPRequest(""), MalformedRequestException) << "Must have at least a command";
-  EXPECT_THROW(HTTPRequest("POST / HTTP/1.1\r\nHost: aoeaoeu\r\n\r"), MalformedRequestException) 
-    << "Needs to end with \r\n\r\n";
-  EXPECT_THROW(HTTPRequest("GET / HTTp/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException) 
-    << "Must be HTTP/1.1 exactly";
-  EXPECT_THROW(HTTPRequest("GeT / HTTP/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException) 
-    << "Must be recognized command (GET/POST/HEAD only)";
-  EXPECT_THROW(HTTPRequest("GET  / HTTP/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException) 
-    << "Only one space separating components in the command!";
-  EXPECT_THROW(HTTPRequest("GET /  HTTP/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException) 
-    << "Only one space separating!";
-  EXPECT_THROW(HTTPRequest("GET / HTTP/1.1\r\nHost: aoeaoeu\r\nhost: aoeu\r\n\r\n"), MalformedRequestException) 
-    << "No duplicate headers!";
+  EXPECT_THROW(HTTPRequest("POST / HTTP/1.1\r\nHost: aoeaoeu\r\n\r"), MalformedRequestException)
+      << "Needs to end with \r\n\r\n";
+  EXPECT_THROW(HTTPRequest("GET / HTTp/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException)
+      << "Must be HTTP/1.1 exactly";
+  EXPECT_THROW(HTTPRequest("GeT / HTTP/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException)
+      << "Must be recognized command (GET/POST/HEAD only)";
+  EXPECT_THROW(HTTPRequest("GET  / HTTP/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException)
+      << "Only one space separating components in the command!";
+  EXPECT_THROW(HTTPRequest("GET /  HTTP/1.1\r\nHost: aoeaoeu\r\n\r\n"), MalformedRequestException)
+      << "Only one space separating!";
+  EXPECT_THROW(HTTPRequest("GET / HTTP/1.1\r\nHost: aoeaoeu\r\nhost: aoeu\r\n\r\n"), MalformedRequestException)
+      << "No duplicate headers!";
 }
